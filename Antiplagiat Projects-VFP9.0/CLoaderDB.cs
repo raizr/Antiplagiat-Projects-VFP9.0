3593f63538c6209506dfb07b15fd7491466b519b
raizr <deni-hottab2@hotmail.com>
2017-03-10 10:18:20 +0300

    Add CLoaderDB, CManager, Form1.resx

---
+++ Antiplagiat Projects-VFP9.0/CLoaderDB.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+using System.IO;
+
+namespace Antiplagiat_Projects_VFP9._0 {
+    class CLoaderDB {
+        public CLoaderDB() {
+            
+        }
+        public CLoaderDB(string FileName) {
+            oleDbConnection = new OleDbConnection("Provider=VFPOLEDB.1;"+
+                                                  "Data Source=" + FileName);
+            oleDbConnection.Open();
+            Console.WriteLine("ServerVersion: {0} \nDatabase: {1}",
+                              oleDbConnection.ServerVersion, oleDbConnection.DataSource);
+
+            if (oleDbConnection.State == ConnectionState.Open) {
+                //AllTables = oleDbConnection.GetSchema("Tables", new string[] { null, null, null, "TABLE" });
+                
+                
+                OleDbDataAdapter DataAdapter = new OleDbDataAdapter("select * from " +
+                    Path.GetFileName(FileName), oleDbConnection);
+                DataSet ds = new DataSet();
+                DataAdapter.Fill(ds);
+                AllTables = ds.Tables[0];
+                //string mySQL = "USE ?";
+                //OleDbCommand query = new OleDbCommand(mySQL, oleDbConnection);
+
+                //DA.SelectCommand = query;
+
+                //DA.Fill(AllTables);
+
+                oleDbConnection.Close();
+            }
+        }
+
+        public DataTable OpenDB(string FileName) {
+            oleDbConnection = new OleDbConnection("Provider=VFPOLEDB.1;" +
+                                                 "Data Source=" + FileName);
+            oleDbConnection.Open();
+            Console.WriteLine("ServerVersion: {0} \nDatabase: {1}",
+                              oleDbConnection.ServerVersion, oleDbConnection.DataSource);
+
+            if (oleDbConnection.State == ConnectionState.Open) {
+                //AllTables = oleDbConnection.GetSchema("Tables", new string[] { null, null, null, "TABLE" });
+
+
+                OleDbDataAdapter DataAdapter = new OleDbDataAdapter("select * from " +
+                    Path.GetFileName(FileName), oleDbConnection);
+                DataSet ds = new DataSet();
+                DataAdapter.Fill(ds);
+                oleDbConnection.Close();
+                return ds.Tables[0];
+                //string mySQL = "USE ?";
+                //OleDbCommand query = new OleDbCommand(mySQL, oleDbConnection);
+
+                //DA.SelectCommand = query;
+
+                //DA.Fill(AllTables);
+
+
+            }else {
+                return null;
+            }
+        }
+
+        ~CLoaderDB() {
+
+        }
+
+        private OleDbConnection oleDbConnection;
+        private DataTable AllTables = new DataTable();
+        public DataTable Table
+        {
+            get
+            {
+                return AllTables;
+            }
+        }
+    }
+}
---
+++ Antiplagiat Projects-VFP9.0/CManager.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Antiplagiat_Projects_VFP9._0 {
+    class CManager {
+        public CManager() {
+
+        }
+        
+        private void CreateSHA1Prj() {
+            HashTables = new string[tablesFullName.Length];
+            HashForms = new string[formsFullName.Length];
+            for (int i = 0; i < tablesFullName.Length; i++) {
+                FileStream stream = File.OpenRead(tablesFullName[i]);
+                byte[] hash = sha1.ComputeHash(stream);
+                //Console.Write(BitConverter.ToString(hash).Replace("-", String.Empty));
+                HashTables[i] = BitConverter.ToString(hash).Replace("-", String.Empty);
+                //Console.Write(HashTables[i]+"\n");
+            }
+            for (int i = 0; i < formsFullName.Length; i++) {
+                FileStream stream = File.OpenRead(formsFullName[i]);
+                byte[] hash = sha1.ComputeHash(stream);
+                //Console.Write(BitConverter.ToString(hash).Replace("-", String.Empty));
+                HashForms[i] = BitConverter.ToString(hash).Replace("-", String.Empty);
+            }
+
+        }
+        public void OpenProject(string[] TablesFullName, string[] FormsFullName) {
+            tablesFullName = TablesFullName;
+            formsFullName = FormsFullName;
+            tablesTables = new DataTable[tablesFullName.Length];
+            formTables = new DataTable[formsFullName.Length];
+            for (int i = 0; i< tablesFullName.Length; i++) {
+                OpenTables(i);
+            }
+            for (int i = 0; i < formsFullName.Length; i++) {
+                OpenForms(i);
+            }
+            CreateSHA1Prj();
+        }
+        public DataTable OpenTables(int i) {
+            //string DirectoryName = Path.GetDirectoryName(tablesFullName[0]);
+            TablesTables[i] = loaderDb.OpenDB(tablesFullName[i]);
+            return TablesTables[i];
+        }
+        public DataTable OpenForms(int i) {
+            //string DirectoryName = Path.GetDirectoryName(tablesFullName[0]);
+            FormTables[i] = loaderDb.OpenDB(formsFullName[i]);
+            return FormTables[i];
+        }
+        ~CManager() {
+
+        }
+        private string[] tablesFullName;
+        public string[] TablesFullName
+        {
+            set
+            {
+                tablesFullName = value;
+            }
+            get
+            {
+                return tablesFullName;
+            }
+        }
+        private string[] formsFullName;
+        public string[] FormsFullName
+        {
+            set
+            {
+                formsFullName = value;
+            }
+        }
+        private SHA1 sha1 = new SHA1Managed();
+        private string[] HashTables;
+        private string[] HashForms;
+        private CLoaderDB loaderDb = new CLoaderDB();
+        private DataTable[] tablesTables;
+        public DataTable[] TablesTables
+        {
+            get
+            {
+                return tablesTables;
+            }
+        }
+        private DataTable[] formTables;
+        public DataTable[] FormTables
+        {
+            get
+            {
+                return formTables;
+            }
+        }
+    }
+}
